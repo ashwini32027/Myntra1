d974ea763e8fb148a418c3b45e3685838adb8ba5
Ashif khan <khanashif6265@gmail.com>
2026-03-09 16:42:13 +0530

    update src/test/java

--- src/test/java/tests/HomepagesTest.java
+++ src/test/java/tests/HomepagesTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import base.Base;
 import pages.HomePage;
 
-public class Hometest extends Base {
+public class HomepagesTest extends Base {
 	HomePage ok;
 	@BeforeMethod
 	public void intialize()
