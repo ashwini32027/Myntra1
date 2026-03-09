cd5e1a7aa6ff971f26f021d9bb652c08fb342576
JAY RAJ <jayraj@Jays-MacBook-Air.local>
2026-03-09 17:17:46 +0530

    change base

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -2,11 +2,18 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class Base {
-	 public WebDriver driver;
+ public WebDriver driver;
+	
+	public Base(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver,this);
+	}
+
 	
 @BeforeMethod
 public void setup()
